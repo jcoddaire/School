ca84313ba37a0e08f3ffc7c4677c00bd7b339258
Jake Coddaire <JCoddaire@grcontrols.com>
2018-12-13 16:50:39 -0500

    Added basic unit tests.

---
+++ School.Tests/Unit/Data/PeopleRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using School.DTOs;
+using School.Data;
+using Moq;
+using School.Tests.Unit.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace School.Tests.Unit.Data
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class PeopleRepository
+    {
+        [TestMethod]
+        public void GetAllPeople_Valid()
+        {
+            var people = PeopleHelper.GetPeopleList().AsQueryable();
+            
+            var mockSet = new Mock<DbSet<Person>>();
+            mockSet.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(people.Provider);
+            mockSet.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(people.Expression);
+            mockSet.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(people.ElementType);
+            //mockSet.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(0 => people.GetEnumerator());
+
+            var mockContext = new Mock<SchoolContext>();
+            mockContext.Setup(c => c.Person).Returns(mockSet.Object);
+
+            var service = new SchoolServiceRepository(mockContext.Object);
+            List<PersonDTO> result = service.GetAllPersons().ToList();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("Amber", result[0].FirstName);
+            Assert.AreEqual("Ben", result[1].FirstName);
+            Assert.AreEqual("Ronald", result[2].FirstName);
+
+            Assert.AreEqual("Baar", result[0].LastName);
+            Assert.AreEqual("Xavier", result[1].LastName);
+            Assert.AreEqual("Weasley", result[2].LastName);
+
+            Assert.AreEqual(3, result[0].PersonID);
+            Assert.AreEqual(1, result[1].PersonID);
+            Assert.AreEqual(5, result[2].PersonID);
+        }
+    }
+}
---
+++ School.Tests/Unit/Helpers/TestPeopleAppContext.cs
@@ -0,0 +1,206 @@
+﻿using School.Data;
+using School.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace School.Tests.Unit.Helpers
+{
+    [ExcludeFromCodeCoverage]
+    public class TestPeopleAppContext : ISchoolData
+    {
+        public List<PersonDTO> People { get; set; }
+
+        public TestPeopleAppContext()
+        {
+            People = new List<PersonDTO>();
+        }
+
+        public IEnumerable<PersonDTO> GetAllPersons()
+        {
+            return People;
+        }
+
+        public PersonDTO GetPerson(int personID)
+        {
+            return People.Where(x => x.PersonID == personID).FirstOrDefault();
+        }
+
+        public PersonDTO CreatePerson(PersonDTO person)
+        {
+            person.PersonID = People.Max(x => x.PersonID) + 1;
+
+            People.Add(person);
+
+            return person;
+        }
+
+        public PersonDTO UpdatePerson(PersonDTO person)
+        {
+            People.Remove(People.Where(x => x.PersonID == person.PersonID).First());
+            People.Add(person);
+
+            return person;
+        }
+
+        public int DeletePerson(int personID)
+        {
+            if(People.Remove(People.Where(x => x.PersonID == personID).First()))
+            {
+                return 1;
+            }
+            return -1;
+        }
+
+        public IEnumerable<DepartmentDTO> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DepartmentDTO GetDepartment(int departmentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DepartmentDTO CreateDepartment(DepartmentDTO department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DepartmentDTO UpdateDepartment(DepartmentDTO department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteDepartment(int departmentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CourseDTO> GetAllCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseDTO GetCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseDTO CreateCourse(CourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseDTO UpdateCourse(CourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }      
+
+        OfficeAssignmentDTO ISchoolData.GetOfficeAssignment(int personID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OfficeAssignmentDTO> GetAllOfficeAssignments()
+        {
+            throw new NotImplementedException();
+        }
+
+        OfficeAssignmentDTO ISchoolData.CreateOfficeAssignment(OfficeAssignmentDTO assignment)
+        {
+            throw new NotImplementedException();
+        }
+
+        OfficeAssignmentDTO ISchoolData.UpdateOfficeAssignment(OfficeAssignmentDTO assignment)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ISchoolData.DeleteOfficeAssignment(int personID)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public StudentGradeDTO GetStudentGrade(int enrollmentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<StudentGradeDTO> GetAllStudentGrades()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentGradeDTO AddStudentGrade(StudentGradeDTO grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentGradeDTO UpdateStudentGrade(StudentGradeDTO grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteStudentGrade(int enrollmentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OnlineCourseDTO> GetAllOnlineCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnlineCourseDTO GetOnlineCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnlineCourseDTO AddOnlineCourse(OnlineCourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnlineCourseDTO UpdateOnlineCourse(OnlineCourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteOnlineCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OnsiteCourseDTO> GetAllOnsiteCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnsiteCourseDTO GetOnsiteCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnsiteCourseDTO AddOnsiteCourse(OnsiteCourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnsiteCourseDTO UpdateOnsiteCourse(OnsiteCourseDTO course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteOnsiteCourse(int courseID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
