5af8b94c8c046ea5330c7e7b5d56a74ec62c6859
Jacob Coddaire <jacobcoddaire+github@gmail.com>
2018-12-30 18:50:34 -0500

    Added swagger.

--- School.API/Startup.cs
+++ School.API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using School.Data;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace School.API
 {
@@ -34,7 +35,14 @@ namespace School.API
 
             services.AddScoped<ISchoolData, SchoolServiceRepository>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "School Service", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +58,18 @@ namespace School.API
             }
 
             app.UseHttpsRedirection();
+            
+            //ENABLE SWAGGER:
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "School Service V1");
+            });
+                       
             app.UseMvc();
         }
     }
